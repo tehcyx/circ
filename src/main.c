c690c68382b068a37d4ac785cd0a91376aa20f5d
Daniel Roth <daniel.roth@arvatosystems.com>
2015-03-31 18:52:38 -0700

    some initial things to start from

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <math.h>       /* for cos(), sin(), and sqrt() */
+
+/* Define port to use */
+#ifndef CIRC_PORT
+#define CIRC_PORT 6665
+#endif
+
+
+int main(int argc, char **argv) {
+
+
+	return 0;             /* ANSI C requires main to return int. */
+}
